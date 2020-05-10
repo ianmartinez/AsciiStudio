d630933628840b53ae5d0fa7760497b09efaa7b1
Ian Martinez <ianmartinez97@live.com>
2020-05-10 13:38:43 -0500

    Change GifFile license

--- src/giflib/GifFile.java
+++ src/giflib/GifFile.java
@@ -1,22 +1,15 @@
-/*
- * Copyright (C) 2020 Ian Martinez
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// GifFile.java
+//  
+// Created by Ian Martinez, May 2020.
+//
+// This work is licensed under the Creative Commons Attribution 3.0 Unported
+// License. To view a copy of this license, visit
+// http://creativecommons.org/licenses/by/3.0/ or send a letter to Creative
+// Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
 package giflib;
 
 /**
+ * Simplifies the opening and saving of GIFs.
  *
  * @author Ian Martinez
  */
