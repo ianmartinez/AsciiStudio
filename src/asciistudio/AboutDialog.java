f9310cac0101ea63cef9dfd20c7c16daab511cf1
Ian Martinez <ianmartinez97@live.com>
2020-05-27 14:52:21 -0500

    Button links now work

--- src/asciistudio/AboutDialog.java
+++ src/asciistudio/AboutDialog.java
@@ -16,6 +16,12 @@
  */
 package asciistudio;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Ian Martinez
@@ -185,59 +191,25 @@ public class AboutDialog extends javax.swing.JDialog {
     }// </editor-fold>//GEN-END:initComponents
 
     private void websiteButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_websiteButtonActionPerformed
-        // TODO add your handling code here:
+        openLink("https://ianmtz.com/asciistudio");
     }//GEN-LAST:event_websiteButtonActionPerformed
 
     private void githubButonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_githubButonActionPerformed
-        // TODO add your handling code here:
+        openLink("https://github.com/ianmartinez/AsciiStudio");
     }//GEN-LAST:event_githubButonActionPerformed
 
     private void patreonButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_patreonButtonActionPerformed
-        // TODO add your handling code here:
+        openLink("https://www.patreon.com/ianmartinez");
     }//GEN-LAST:event_patreonButtonActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+    private void openLink(String url) {
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AboutDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AboutDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AboutDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AboutDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (URISyntaxException | IOException ex) {
+            JOptionPane.showMessageDialog(this, "Could not open '" + url + "'");            
         }
-        //</editor-fold>
-
-        /* Create and display the dialog */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                AboutDialog dialog = new AboutDialog(new javax.swing.JFrame(), true);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    @Override
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
-            }
-        });
     }
-
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel appNameLabel;
     private javax.swing.JPanel buttonPanel;
