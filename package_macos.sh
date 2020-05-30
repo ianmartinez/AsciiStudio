399e7475257dd94b9d47a4ddc66ed77f1b098439
Ian Martinez <ianmartinez97@live.com>
2020-05-30 15:35:53 -0500

    Release macOS version

--- package_macos.sh
+++ package_macos.sh
@@ -1,6 +1,10 @@
-jpackage --input dist/ \
+jpackage --input 'dist/' \
   --name 'ASCII Studio' \
-  --main-jar AsciiStudio.jar \
-  --main-class asciistudio.AsciiStudio \
-  --type dmg \
-  --java-options '--enable-preview'
+  --main-jar 'AsciiStudio.jar' \
+  --main-class 'asciistudio.AsciiStudio' \
+  --icon 'icon/icon.icns' \
+  --type 'dmg' \
+  --java-options '--enable-preview' \
+  --app-version '4.0' \
+  --copyright 'Copyright © 2016-2020 Ian Martinez' \
+  --dest 'release/'
